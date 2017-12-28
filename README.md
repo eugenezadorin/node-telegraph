b5c221d8d338fb651277c395a06bf8845fd67fc8
Eugene Zadorin <mail@eugenezadorin.ru>
2017-12-28 17:55:11 +0300

    using app secret as env variable

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
     cd node-telegraph
     npm install
 
+    # setup your app secret in .env file
+    mv .env.example .env
+
     # start app on http://localhost:3000/
     npm run server
 
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,16 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 
-// @todo: move to configuration
-const appSecret = '79f210e028a13d9e21a938972ecedd264708da9a0b7a93b3c2669bada7ce96f1501eeda3ade501606595558ab0b95db2ba11e2a945358082251b8223925eb173';
-
-
 app.use(bodyParser.json());
 
-app.use(cookieParser(appSecret));
+app.use(cookieParser(process.env.APP_SECRET));
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
